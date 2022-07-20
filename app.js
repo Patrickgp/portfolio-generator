24a42d11fbab9cda2a1cc9d8d19672b7c164e118
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-07-20 18:39:53 -0400

    modify the html template to handle more data

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-// const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage(portfolioData);
 
 // fs.writeFile("index.html", pageHTML, (err) => {
-//   if (err) throw err;
+// //   if (err) throw err;
 
 //   console.log("Portfolio complete! Check out index.html to see the output!");
 // });
@@ -119,5 +119,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile("index.html", pageHTML, (err) => {
+      if (err) throw err;
+      console.log(
+        "Portfolio complete! Check out index.html to see the output!"
+      );
+    });
   });
