b0433b02b35aae5028d404b4bcf1dee016fabd57
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-07-21 09:31:31 -0400

    increase data provided to HTML file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const generatePage = require("./src/page-template.js");
 // const pageHTML = generatePage(portfolioData);
 
 // fs.writeFile("index.html", pageHTML, (err) => {
-// //   if (err) throw err;
+//   if (err) throw err;
 
 //   console.log("Portfolio complete! Check out index.html to see the output!");
 // });
